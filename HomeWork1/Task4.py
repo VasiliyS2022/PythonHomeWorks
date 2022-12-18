a744e57a9f01abc930922f7901f46f4eb119a263
VS <vskovrov@yandex.ru>
2022-12-19 02:43:24 +0300

    Добавил домашнее задание по первому семинару

---
+++ HomeWork1/Task4.py
@@ -0,0 +1,32 @@
+# ДОМАШНЕЕ ЗАДАНИЕ СЕМИНАР1 ЗАДАЧА4
+##########################################################################################################
+
+# Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат 
+# точек в этой четверти (x и y).
+
+import random
+
+def QuartNum(num):
+    if num == '1': # a > 0 and b > 0
+        a = random.randrange(1, 50)
+        b = random.randrange(1, 50)
+    elif num == '2': # a < 0 and b > 0
+        a = random.randrange(-50, -1)
+        b = random.randrange(1, 50)
+    elif num == '3': # a < 0 and b < 0
+        a = random.randrange(-50, -1)
+        b = random.randrange(-50, -1)
+    elif num == '4': # a > 0 and b < 0
+        a = random.randrange(1, 50)
+        b = random.randrange(-50, -1)
+    else:
+        a = 0
+        b = 0
+    return a, b
+
+numQuart = input('Введите номер четверти от 1 до 4: ')
+
+quart = QuartNum(numQuart)
+print(f'В четверти {numQuart} значения точек A и B могут быть равны {quart}')
+
+##########################################################################################################
