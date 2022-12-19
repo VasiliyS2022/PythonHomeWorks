8e5a5d5d95c1875720068a1f413f6e8f3bfbb790
VS <vskovrov@yandex.ru>
2022-12-19 16:05:37 +0300

    Сделал изменение в задаче 2

--- HomeWork1/Task2.py
+++ HomeWork1/Task2.py
@@ -4,24 +4,13 @@
 # (!!!Доп!!!) Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех 
 # значений предикат.
 
-def Meaning(x, y, z):
-        if x == '1': x = True
-        elif x == '0': x = False
-        if y == '1': y = True
-        elif y == '0': y = False
-        if z == '1': z = True
-        elif z == '0': z = False
+def Checking(i, j, k):
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                print(f'({i},{j},{k}) {(not(i or j or k)) == ((not i) and (not j) and (not k))}')
 
-        if ((not(x or y or z)) == ((not x) and (not y) and (not z))): flag = True
-        else: flag = False
-        return flag
-
-numX = input('Введите значение X(0 или 1): ')
-numY = input('Введите значение Y(0 или 1): ')
-numZ = input('Введите значение Z(0 или 1): ')
-if ((numX == '0' or numX == '1') and (numY == '0' or numY == '1') and (numZ == '0' or numZ == '1')):
-    result = Meaning(numX, numY, numZ)
-    print(result)
-else: print("Введены ошибочные данные, попробуйте еще раз!")
+numI, numJ, numK = 0, 0, 0
+res = Checking(numI, numJ, numK)
 
 ##########################################################################################################
