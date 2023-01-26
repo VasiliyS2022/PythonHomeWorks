90bff5f6d07f7c5d89bdc6aeb7090909ee595bfd
VS <vskovrov@yandex.ru>
2023-01-27 01:27:01 +0300

    Добавил домашнее задание по седьмому семинару

--- HomeWork7/modelWrite.py
+++ HomeWork7/modelWrite.py
@@ -4,4 +4,4 @@ def WriteValuesColumn(values):
 
 def WriteValuesRow(values):
     with open('2.txt', 'a', encoding='utf-8') as file:
-        file.write(f'Фамилия: {str(values[0])} \n  Имя: {str(values[1])} \n Телефон: {str(values[2])} \n Описание: {str(values[3])} \n\n')
+        file.write(f'Фамилия: {str(values[0])} \nИмя: {str(values[1])} \nТелефон: {str(values[2])} \nОписание: {str(values[3])} \n\n')
