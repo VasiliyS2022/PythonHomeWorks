0e01fd04186a3d62426bfe3bb81f4769c69f0e54
VS <vskovrov@yandex.ru>
2023-01-27 01:11:58 +0300

    Добавил домашнее задание по седьмому семинару

--- HomeWork7/modelRead.py
+++ HomeWork7/modelRead.py
@@ -1,7 +1,7 @@
 def ViewColumn():
-    with open('1.txt', 'r') as file:
+    with open('1.txt', 'r', encoding='utf-8') as file:
         print(*file)
 
 def ViewRow():
-    with open('2.txt', 'r') as file:
+    with open('2.txt', 'r', encoding='utf-8') as file:
         print(*file)
